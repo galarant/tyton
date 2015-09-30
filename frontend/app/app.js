9970c56c44070590261c8032c48995e95f6e813f
Jeff <jeff.revesz@buzzfeed.com>
2015-09-29 21:03:30 -0400

    resolves #2

---
+++ frontend/app/app.js
@@ -0,0 +1,17 @@
+require("angular/angular");
+require("angular-route/angular-route.js");
+
+angular.module("tyton", [
+  "ngRoute"
+  ])
+  .config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "/static/home/home.html"
+      })
+      .otherwise({
+        redirectTo: "/"
+      });
+  }]);
+
+require("./home/home-controller.js");
---
+++ frontend/app/home/home-controller.js
@@ -0,0 +1,5 @@
+function HomeController ($location) {}
+
+angular.module("tyton").controller("HomeController", [
+  HomeController
+]);
