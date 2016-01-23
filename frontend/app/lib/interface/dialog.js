76257e84164b3d1b4667e49c610beeed20a7e41f
Jeff <jeff.revesz@buzzfeed.com>
2016-01-23 17:23:59 -0500

    resolves #65

--- frontend/app/lib/interface/dialog.js
+++ frontend/app/lib/interface/dialog.js
@@ -1,28 +1,50 @@
-import { Phrase } from './phrase';
+import _ from "lodash";
+import { Phrase } from "./phrase";
 
-class Dialog {
+class Dialog extends Phaser.Group {
 
-  constructor(game, strings=[], font_size=150, frames=200) {
-    this.strings = strings;
+  constructor(camera, strings=[], font_size=60,
+    fade_in_speed=Phaser.Timer.SECOND * 2.0, camera_offset=null) {
+
+    super(camera.game, camera.world);
+
+    //group attributes
+    this.camera = camera;
     this.font_size = font_size;
-    this.frames = frames;
-    this.game = game;
+    this.fade_in_speed = fade_in_speed;
+    this.phrases = _.map(strings, this.string_to_phrase, this);
+    this.submit_signal = new Phaser.Signal();
+
+    this.fixedToCamera = true;
+    if (camera_offset) {
+      this.cameraOffset = camera_offset;
+    } else {
+      this.cameraOffset = new Phaser.Point(this.camera.width / 2, this.camera.height / 2);
+    }
     this.current_phrase = null;
+    this.on_down_binding = this.camera.game.input.onDown.add(this.next, this);
+    this.next();
   }
 
   next() {
-    if (this.current_phrase !== null) {
+    if (this.current_phrase) {
       this.current_phrase.destroy();
     }
-    if (this.strings.length > 0) {
-      let next_string = this.strings.shift();
-      this.current_phrase = new Phrase(this.game, next_string, this.font_size, this.frames);
+
+    if (this.phrases.length > 0) {
+      this.current_phrase = this.phrases.shift();
       this.current_phrase.show();
-      return this.current_phrase;
+    } else {
+      this.on_down_binding.detach();
+      this.submit_signal.dispatch();
+      this.destroy();
     }
-    else {
-      return null;
-    }
+  }
+
+  string_to_phrase(string) {
+    let this_phrase = new Phrase(this.camera, string, this.font_size, this.fade_in_speed);
+    this.addChild(this_phrase);
+    return this_phrase;
   }
 
 }
--- frontend/app/lib/sprites/tyton.js
+++ frontend/app/lib/sprites/tyton.js
@@ -1,4 +1,4 @@
-import { Phrase } from 'lib/interface/phrase';
+import { Modal } from 'lib/interface/modal';
 import { Dialog } from 'lib/interface/dialog';
 
 class Tyton extends Phaser.Sprite {
@@ -23,9 +23,6 @@ class Tyton extends Phaser.Sprite {
     //game world attributes
     game.world.add(this);
     game.camera.follow(this);
-
-    //object attributes
-    this.dialog = null;
 
     //key mappings
     this.successKey = game.input.keyboard.addKey(Phaser.KeyCode.S);
@@ -59,33 +56,6 @@ class Tyton extends Phaser.Sprite {
     }
 
     this.check_health();
-  }
-
-  start_speaking(strings) {
-    this.body.static = true;
-    this.speaking_tween = this.game.add.tween(this).to({alpha: 0.4}, 1000, "Linear", true, 0, -1, true);
-    this.dialog = new Dialog(this.game, strings, 150, 100);
-    this.speak();
-  }
-
-  speak() {
-    if (this.dialog !== null) {
-      let next_phrase = this.dialog.next();
-      if (next_phrase === null) {
-        this.stop_speaking();
-      }
-    }
-  }
-
-  stop_speaking() {
-    this.dialog = null;
-    this.speaking_tween.stop();
-    this.game.add.tween(this).to( {alpha: 1}, 125, "Linear", true);
-    this.body.static = false;
-  }
-
-  say(strings) {
-    this.start_speaking(strings);
   }
 
   check_health () {
